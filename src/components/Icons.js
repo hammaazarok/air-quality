7b8f86e28ef95a4d26c4e14ae3498a21ff8d68eb
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-02 23:45:19 +0200

    add react icons used

---
+++ src/components/Icons.js
@@ -0,0 +1,7 @@
+import { BsFillMicFill as Mic, BsArrowRightCircle as Arrow } from 'react-icons/bs';
+import { AiFillSetting as Setting } from 'react-icons/ai';
+import { MdOutlineArrowBackIos as BackArrow } from 'react-icons/md';
+
+export {
+  Mic, Setting, Arrow, BackArrow,
+};
